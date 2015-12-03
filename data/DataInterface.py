4fc3c0a3fe137a183e2e7906adc13e614e82ffbc
RWArunde <filbertrunde@gmail.com>
2015-12-03 17:44:18 -0500

    Create interface for data store module

---
+++ data/DataInterface.py
@@ -0,0 +1,76 @@
+class DataInterface(object):
+    """ This is the interface for the persistent data store
+    backing an edX-adapt course. It stores information about
+    courses, such as which skills they teach and which problems
+    are associated with those skills. It also stores user data,
+    such as which users are currently progressing through a
+    course, which users have finished the course, and logs of
+    the students' interactions with the problems.
+    """
+
+    def __init__(self):
+        do = "nothing"
+
+    """ Course setup methods """
+    def post_course(self, course_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def post_skill(self, course_id, skill_name):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def post_problem(self, course_id, skill_names, problem_name, tutor_url):
+        raise NotImplementedError( "Data module must implement this" )
+        #test problems...?
+
+    def enroll_user(self, course_id, user_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+
+    """ Add user data """
+    def post_interaction(self, course_id, problem_name, user_id, correct):
+        raise NotImplementedError( "Data module must implement this" )
+
+
+    """ Retrieve course information """
+    def get_course_ids(self):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_skills(self, course_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_problems(self, course_id, skill_name):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_in_progress_users(self, course_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_finished_users(self, course_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+
+    """ Retrieve user information """
+    def get_all_remaining_problems(self, course_id, user_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_remaining_problems(self, course_id, skill_id, user_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_all_interactions(self, course_id, user_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_interactions(self, course_id, skill_id, user_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_raw_user_data(self, course_id, user_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    def get_all_raw_data(self, course_id):
+        raise NotImplementedError( "Data module must implement this" )
+
+    """ General backing store access: allows other modules
+        access to persistent storage
+    """
+    def set(self, key, value):
+        raise NotImplementedError( "Data module must implement this" )
+    def get(self, key):
+        raise NotImplementedError( "Data module must implement this" )

ddf837dd89bc90f249a2f2d4caa93344f27f829c
dexter <mhlee1116@gmail.com>
2015-12-05 17:48:20 -0500

    SelectException

--- select/interface.py
+++ select/interface.py
@@ -1,3 +1,6 @@
+from data.interface import DataInterface
+from model.interface import ModelInterface
+
 class SelectInterface(object):
     """ This is the interface for the adaptive problem
     selection logic. It interacts with the inputted data interface and
@@ -5,8 +8,16 @@ class SelectInterface(object):
     not directly store any information about the user or the course.
     """
 
+    data_interface = None # Data module which gets the state information
+    model_interface = None # Student model
+
     def __init__(self, data_interface, model_interface):
-        do = "nothing"
+        self.data_interface = data_interface
+        self.model_interface = model_interface
 
     def choose_next_problem(self, course_id, user_id):
         raise NotImplementedError( "Data module must implement this" )
+
+
+class SelectException(Exception):
+    pass

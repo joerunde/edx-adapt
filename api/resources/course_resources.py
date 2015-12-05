f0b6d995f09b2f7bce8af7a7ae3cca8140a0370e
RWArunde <filbertrunde@gmail.com>
2015-12-05 17:00:11 -0500

    add resource files for data serving and course management

---
+++ api/resources/course_resources.py
@@ -0,0 +1,4 @@
+""" This file contains api resource classes dealing with course information.
+For example, CRUDding courses, users, problems, skills...
+"""
+
---
+++ api/resources/data_serve_resources.py
@@ -0,0 +1,6 @@
+""" This file contains api resources for serving data from the course.
+Currently this is for downloading user response data, but this could be
+extended for viewing model parameter data in a web gui, etc.
+"""
+
+

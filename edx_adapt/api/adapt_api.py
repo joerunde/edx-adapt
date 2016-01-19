2bd046846d5acd1521f3ce5c5d18c858a6204db2
RWArunde <filbertrunde@gmail.com>
2016-01-19 17:42:35 -0500

    add selector and model to api definition

--- edx_adapt/api/adapt_api.py
+++ edx_adapt/api/adapt_api.py
@@ -8,6 +8,8 @@ import resources.course_resources as CR
 import resources.tutor_resources as TR
 # import data module
 import edx_adapt.data.tinydbRepository as repo
+import edx_adapt.select.skill_separate_random_selector as select
+import edx_adapt.model.bkt as bkt
 
 app = Flask(__name__)
 CORS(app)
@@ -16,23 +18,25 @@ api = Api(app)
 # TODO: load from settings
 base = '/api/v1'
 
-data = repo.TinydbRepository('/tmp/2.json')
+database = repo.TinydbRepository('/tmp/2.json')
+student_model = bkt.BKT
+selector = select.SkillSeparateRandomSelector(database, student_model)
 
 api.add_resource(CR.Courses, base + '/course',
-                 resource_class_kwargs={'data': data})
+                 resource_class_kwargs={'data': database})
 api.add_resource(CR.Skills, base + '/course/<course_id>/skill',
-                 resource_class_kwargs={'data': data})
+                 resource_class_kwargs={'data': database})
 api.add_resource(CR.Users, base + '/course/<course_id>/user',
-                 resource_class_kwargs={'data': data})
+                 resource_class_kwargs={'data': database})
 api.add_resource(CR.Problems, base + '/course/<course_id>', base + '/course/<course_id>/skill/<skill_name>',
-                 resource_class_kwargs={'data': data})
+                 resource_class_kwargs={'data': database})
 
 api.add_resource(TR.UserInteraction, base + '/course/<course_id>/user/<user_id>/interaction',
-                 resource_class_kwargs={'data': data})
+                 resource_class_kwargs={'data': database})
 api.add_resource(TR.UserProblems, base + '/course/<course_id>/user/<user_id>',
-                 resource_class_kwargs={'data': data})
+                 resource_class_kwargs={'data': database})
 api.add_resource(TR.UserPageLoad, base + '/course/<course_id>/user/<user_id>/pageload',
-                 resource_class_kwargs={'data': data})
+                 resource_class_kwargs={'data': database})
 
 
 @app.errorhandler(404)  # Return JSON with 404 instead of html
